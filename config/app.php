83427515cc75d9cc18777688d4deca2a03f5fc10
Asai0307 <asaken0307@gmail.com>
2025-03-31 21:01:29 +0900

    provvider 変更

--- config/app.php
+++ config/app.php
@@ -123,26 +123,26 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
     
-    'providers' => [
+    // 'providers' => [
         /*
          * Laravel Framework Service Providers...
          */
-        Illuminate\Auth\AuthServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BusServiceProvider::class,
-        Illuminate\Cache\CacheServiceProvider::class,
-        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\Database\DatabaseServiceProvider::class,
-        Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
-        Illuminate\Log\LogServiceProvider::class,
-        Illuminate\Notifications\NotificationServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        // Illuminate\Auth\AuthServiceProvider::class,
+        // Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        // Illuminate\Bus\BusServiceProvider::class,
+        // Illuminate\Cache\CacheServiceProvider::class,
+        // Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+        // Illuminate\Validation\ValidationServiceProvider::class,
+        // Illuminate\Database\DatabaseServiceProvider::class,
+        // Illuminate\Session\SessionServiceProvider::class,
+        // Illuminate\Translation\TranslationServiceProvider::class,
+        // Illuminate\Filesystem\FilesystemServiceProvider::class,
+        // Illuminate\Log\LogServiceProvider::class,
+        // Illuminate\Notifications\NotificationServiceProvider::class,
+        // Illuminate\Broadcasting\BroadcastServiceProvider::class,
         // 他のプロバイダーも追加可能...
 
         // 自分で作成したプロバイダー
-        App\Providers\RouteServiceProvider::class,
-    ],
+        // App\Providers\RouteServiceProvider::class,
+    // ],
 ];
